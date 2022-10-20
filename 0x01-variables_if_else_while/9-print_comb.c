745978bece89e12e99c0638a5a6edc9172549d90
Richard <rrodrot@gmail.com>
2022-10-20 10:37:24 -0700

    Print combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all combinations of single digit numbers
+ *
+ * Retun: Always 0
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar((num % 10) + '0');
+		if (num == 9)
+			continue;
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
