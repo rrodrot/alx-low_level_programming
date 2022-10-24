a149466de06df0d351e4cdc82ed00440dbab5422
Richard <rrodrot@gmail.com>
2022-10-24 09:07:30 -0700

    Print 10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,16 +10,16 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 15; j++)
 		{
 			if (j >= 10)
 			{
-				putchar((j / 10) + '0');
+				_putchar((j / 10) + '0');
 			}
-			putchar((j % 10) + '0');
+			_putchar((j % 10) + '0');
 		}
-		putchar(10);
+		_putchar(10);
 	}
 }
